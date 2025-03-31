56470ee22546d44e64d26da062426d141db8b40a
Dave Mainville <dave@davemainville.com>
2025-03-30 21:15:36 -0400

    chore: upgrade libs

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -4,7 +4,7 @@ import { defineAuth } from '@aws-amplify/backend';
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
-export const auth = defineAuth({
+export const auth: ReturnType<typeof defineAuth> = defineAuth({
   loginWith: {
     email: true,
   },
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -16,7 +16,7 @@ const schema = a.schema({
 
 export type Schema = ClientSchema<typeof schema>;
 
-export const data = defineData({
+export const data: ReturnType<typeof defineData> = defineData({
   schema,
   authorizationModes: {
     defaultAuthorizationMode: "apiKey",
@@ -32,7 +32,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
