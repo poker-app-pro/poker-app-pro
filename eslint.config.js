e60178aad094f27dd4b64253bf6b0424a5e7d9d3
Dave Mainville <dave@davemainville.com>
2025-04-01 19:48:53 -0400

    chore: ignore .amplify

--- eslint.config.js
+++ eslint.config.js
@@ -15,7 +15,7 @@ export default [
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '.amplify'],
   },
 
   ...pluginVue.configs['flat/recommended'],
